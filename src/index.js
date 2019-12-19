7e832b814f2d82f55896da32dcb54a8ce484a852
Karolis Vilpisauskas <karolis.vilpisauskas@gmail.com>
2019-12-19 21:17:26 +0200

    - set axios defaults

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import App from "./App";
+import axios from 'axios';
 import { Provider } from "react-redux";
 import store from "./store/store";
 import * as serviceWorker from "./serviceWorker";
+
+axios.defaults.baseURL = "https://rickandmortyapi.com/api/";
+axios.defaults.headers.common["Content-Type"] = "application/json";
 
 ReactDOM.render(
   <Provider store={store}>
